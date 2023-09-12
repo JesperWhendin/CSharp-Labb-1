5d81d67f99dcc9bc3e4c21d2ea9b8d32bde50971
JesperWhendin <143404976+JesperWhendin@users.noreply.github.com>
2023-09-12 23:30:28 +0200

    Labb 1 12-9-23 23:30

---
+++ Labb 1/Program.cs
@@ -0,0 +1,89 @@
+﻿void Labb1()
+{
+    {
+        // Labb 1 – Hitta tal i sträng med tecken
+
+        // Skapa en konsollapplikation som tar en textsträng(string) som argument till
+        // Main eller uppmanar användaren mata in en sträng i konsollen.
+        // Textsträngen ska sedan sökas igenom efter alla delsträngar som är tal som börjar
+        // och slutar på samma siffra, utan att start/ slutsiffran, eller något annat tecken än
+        // siffror förekommer där emellan.
+        // ex. 3463 är ett korrekt sådant tal, men 34363 är det inte eftersom det finns
+        // ytterligare en 3:a i talet, förutom start och slutsiffran. Strängar med bokstäver i
+        // t.ex 95a9 är inte heller ett korrekt tal.
+
+        // Skriv ut och markera varje korrekt delsträng
+        // För varje sådan delsträng som matchar kriteriet ovan ska programmet skriva ut en
+        // rad med hela strängen, men där delsträngen är markerad i en annan färg.
+        // Exempel output för input ”29535123p48723487597645723645”:
+
+
+        // Byt färg genom Console.ForegroundColor.
+
+        // Addera ihop alla tal och skriv ut totalen
+        // Programmet ska också addera ihop alla tal den hittat enligt ovan och skriva ut det
+        // sist i programmet. Gör gärna en tom rad emellan för att skilja från output ovan.
+        // Exempel output för input ”29535123p48723487597645723645”:
+        // Total = 5836428677242
+
+        // Redovisning
+        // Uppgiften ska lösas individuellt.
+        // Lämna in uppgiften på ithsdistans med en kommentar med github-länken.
+    } // Labbeskrivning
+    {
+        // För godkänt:
+        // * Räcker att lösa en av uppgifterna, d.v.s.antingen skriva ut alla
+        //   delsträngar som i exemplet ovan, eller räkna ut och skriva ut totalen.
+        // * Om man matar in strängen i exemplet ska man få samma output som ovan.
+        // * Det ska även fungera generellt, oavsett vilken sträng man matar in.
+
+        // För väl godkänt krävs även:
+        // * Båda uppgifterna ska vara lösta, d.v.s.programmet ska först skriva ut alla
+        //   delsträngar som i exemplet ovan och därefter räkna ut och skriva ut total.
+        // * Koden ska vara väl strukturerad och lätt att förstå.
+    } // Betygskriterier
+    {
+        // Be om textsträng
+        //
+
+
+
+
+    } // Psuedokod
+
+    // string[] numbers = new string[10] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    // string[] alfabet = new string[29] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö" };
+
+    // Exempelsträng: 29535123p48723487597645723645
+
+    string inputSequence = string.Empty;
+    string highlightSequence = string.Empty;
+
+    Console.WriteLine("Sträng:");
+    // Console.WriteLine("Vad roligt att du matade in 29535123p48723487597645723645");
+    // string inputSequence = Console.ReadLine();
+    // inputSequence = "29535123p48723487597645723645";
+    inputSequence = "11931g81488";
+
+    for (int i = 0; i < inputSequence.Length; i++)
+    {
+        // highlightSequence += inputSequence[i];
+        if (char.IsDigit(inputSequence[i]))
+        {
+            for (int j = i + 1; j < inputSequence.Length; j++)
+            {
+                if (inputSequence[i] == inputSequence[j])
+                {
+                    Console.WriteLine(inputSequence.Substring(i, j - i + 1));
+                    highlightSequence += inputSequence.Substring(i, j - i + 1);
+                    break;
+                }
+                else if (!char.IsDigit(inputSequence[j]))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+Labb1();
